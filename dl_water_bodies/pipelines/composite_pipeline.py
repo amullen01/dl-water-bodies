fix masking for compositing

dl_water_bodies/pipelines/composite_pipeline.py
@@ -8,35 +8,87 @@ import pandas as pd
 from osgeo import osr
 import rioxarray as rxr
 from multiprocessing import Pool
-
+from glob import glob
 from dl_water_bodies.utils.gs_utils import write_geotiff
-
+import re
+from datetime import datetime
 
 # -----------------------------------------------------------------------------
 # class Composite
 # -----------------------------------------------------------------------------
 class Composite(object):
 
-    def __init__(self, input_dataframe_path: str,
-                 output_dir: str, month: str, nodata: int = 0,
+    def __init__(self, prediction_dir: str, mask_dir: str, years: str, months: str, 
+                 output_dir: str, composite_name: str, nodata: int = 0,
                  logger: logging.Logger = None) -> None:
-
-        self._dataframe = pd.read_csv(input_dataframe_path)
-
+        
+        self._prediction_dir = prediction_dir
+        self._mask_dir = mask_dir
+        
+        self._years = years
+        self._months = months
+        
         self._output_dir = output_dir
-
-        self._month = month
+        self._composite_name = composite_name
 
         self._nodata = nodata
 
-        self._image_paths = self._dataframe['associated_image'].to_list()
-        self._prediction_paths = self._dataframe['prediction_name'].to_list()
-
         self._logger = logger
+        
+        self._prediction_paths = self.get_prediction_filepaths()
+        self._mask_paths = self.get_corresponding_mask_filepaths()
 
     # -------------------------------------------------------------------------
     # build_composite
     # -------------------------------------------------------------------------
+    def get_prediction_filepaths(self) -> list:
+        """i
+        Get filename from list of regexes
+        """
+        # get the paths/filenames of the regex
+        filenames = []
+        print(self._years)
+        print(self._months)
+        for im in os.listdir(self._prediction_dir):
+
+            if im.endswith('.tif'):
+                date=re.search(r'\d{8}', im)
+                timestamp = datetime.strptime(date.group(), '%Y%m%d').date()
+
+                if timestamp.month in self._months:
+                    if timestamp.year in self._years:
+                        print(timestamp)
+                        filenames.append(os.path.join(self._prediction_dir, im))
+
+        return sorted(filenames)
+
+    def get_corresponding_mask_filepaths(self) -> list:
+        """
+        get masks corresponding to predictions, if no mask, will be None
+        """
+        #get all mask dates
+        mask_dates = [re.search(r'\d{8}', d).group() for d in os.listdir(self._mask_dir)]
+        
+        #get full mask paths
+        mask_paths = [os.path.join(self._mask_dir, m) for m in os.listdir(self._mask_dir)]
+    
+        #empty array to fill with mask paths corresponding to prediction paths
+        corresponding_paths = []
+    
+        #iterate predictions
+        for p in self._prediction_paths:
+            date=re.search(r'\d{8}', p).group()
+            
+            if date in mask_dates:
+                idx = mask_dates.index(date) 
+                corresponding_paths.append(mask_paths[idx])
+            
+            else:
+                corresponding_paths.append(None)
+        
+        return corresponding_paths      
+
+   
     def build_composites(self):
 
         min_x, max_x, min_y, max_y = self.get_bounding_box(
@@ -49,40 +101,40 @@ class Composite(object):
         np.save('nb_sum_water.npy', sum_water)
         np.save('nb_num_preds.npy', num_predictions)
 
-        gt = [min_x, 3.125, 0, max_y, 0, -3.125]
+        gt = [min_x, 3.0, 0, max_y, 0, -3.0]
         projection = osr.SpatialReference()
-        projection.ImportFromEPSG(32606)
+        projection.ImportFromEPSG(32603)
         projection = projection.ExportToWkt()
 
         waterprob_path = os.path.join(self._output_dir,
-                                      f'{self._month}prob_composite.tif')
+                                      f'{self._composite_name}_prob_composite.tif')
 
         self._logger.info(f'Writing water prob to: {waterprob_path}')
 
         write_geotiff(
             waterprob_path,
             np.shape(water_prob[0]), gt,
-            projection, water_prob[0], 0)
+            projection, water_prob[0], 10)
 
         npred_path = os.path.join(self._output_dir,
-                                  f'{self._month}num_obs_composite.tif')
+                                  f'{self._composite_name}_num_obs_composite.tif')
 
         self._logger.info(f'Writing num predictions to: {npred_path}')
 
         write_geotiff(
             npred_path,
             np.shape(num_predictions[0]), gt,
-            projection, num_predictions[0], 0)
+            projection, num_predictions[0], 10)
 
         nwater_path = os.path.join(self._output_dir,
-                                   f'{self._month}num_water_composite.tif')
+                                   f'{self._composite_name}_num_water_composite.tif')
 
         self._logger.info(f'Writing num water to: {nwater_path}')
 
         write_geotiff(
             nwater_path,
             np.shape(sum_water[0]), gt,
-            projection, sum_water[0], 0)
+            projection, sum_water[0], 10)
 
         return sum_water, num_predictions, water_prob, min_x, max_y
 
@@ -147,6 +199,7 @@ class Composite(object):
             image_data = src.read(1)
             shape = np.shape(image_data)
             gt = src.get_transform()
+        print(shape, gt)
         return shape, gt
 
     # -------------------------------------------------------------------------
@@ -168,6 +221,7 @@ class Composite(object):
             :return: (sum_water, num_predictions, sum_water/num_predictions)
             :rtype: tuple(float, float, float)
         """
+        
         #initialize empty arrays to build
         sum_water = np.array([])
         num_predictions = np.array([])
@@ -176,16 +230,15 @@ class Composite(object):
 
             self._logger.info(f'Processing {self._prediction_paths[i]}')
 
-            #open image, pad to bounding box extent
-            xds_im = rxr.open_rasterio(self._image_paths[i], nodata=0)
-            xds_im.values = xds_im.values.astype(np.uint8)
-            xds_im = xds_im.rio.pad_box(
+            #open prediction, pad to bounding box extent
+            xds_mask = rxr.open_rasterio(self._mask_paths[i], nodata=0)
+            xds_mask.values = xds_mask.values.astype(np.uint8)
+            xds_mask = xds_mask.rio.pad_box(
                 minx=min_x,
                 miny=min_y,
                 maxx=max_x,
                 maxy=max_y
             )
-
             #open prediction, pad to bounding box extent
             xds_pred =  rxr.open_rasterio(self._prediction_paths[i], nodata=0)
             xds_pred = xds_pred.rio.pad_box(
@@ -196,7 +249,7 @@ class Composite(object):
             )
 
             #get values
-            image_data = np.array(xds_im.values)
+            mask_data = np.array(xds_mask.values)
             xds_im = None
 
             pred_data = np.array(xds_pred.values)
@@ -208,10 +261,11 @@ class Composite(object):
                 num_predictions = np.zeros(pred_data.shape) 
 
             sum_water, num_predictions = self.composite_kernel(
-                i, image_data, pred_data, sum_water, num_predictions,
-                nodata=self._nodata)
+                i, mask_data, pred_data, sum_water, num_predictions)
 
-    
+            mask_data = None
+            pred_data = None
+
         return sum_water, num_predictions, sum_water/num_predictions
 
     # -------------------------------------------------------------------------
@@ -219,8 +273,8 @@ class Composite(object):
     # -------------------------------------------------------------------------
     @staticmethod
     @nb.njit(parallel=True, fastmath=False)
-    def composite_kernel(iteration, image_data, pred_data, sum_water,
-                        num_predictions, nodata=10):
+    def composite_kernel(iteration, mask_data, pred_data, sum_water,
+                        num_predictions):
         """
         Compositing kernel that performs the thresholding and
         gets the total water and number of predictions for each pixel.
@@ -235,44 +289,24 @@ class Composite(object):
             for x in nb.prange(pred_data.shape[2]):
 
                 pred = pred_data[0, y, x]
-
-                if pred == nodata:
-                    pred = np.nan
-
+              
+                #catch any nans or otherwise unexpected values
                 if pred != 1:
-                    if image_data[0, y, x] != 0: 
-                        pred = 0
-
-                pred = np.float32(pred)
-
-                if iteration == 0:
-
-                    sum_water[0, y, x] = pred
-
-                    if pred >= 0:
-                        pred = 1
-                    if np.isnan(pred):
-                        pred = 0
-
-                    pred = np.uint32(pred)
-                    num_predictions[0, y, x] = pred
+                    pred=0
                 
-                else:
+                #pred = np.uint32(pred)
 
-                    sum_water[0, y, x] = np.nansum(
-                        np.array([sum_water[0, y, x], pred])
-                    )
-
-                    if pred >= 0:
-                        pred = 1
-
-                    if pred != 1:
-                        pred = 0
-
-                    pred = np.uint32(pred)
-
-                    num_predictions[0, y, x] = np.nansum(
-                        np.array([num_predictions[0, y, x], pred])
-                    )
+                pixel_valid = 1
+                
+                #valid mask pixel: clear = 1 & snow = 0 & shadow = 0 & haze_light = 0 & haze_heavy = 0 & cloud = 0 
+                #if invalid pixel
+                #if mask_data[0, y, x] != 1 or np.any(mask_data[1:6, y, x]):
+                if mask_data[0, y, x] != 1 or mask_data[1, y, x] != 0 or mask_data[4, y, x] != 0 or mask_data[5, y, x] != 0: 
+                    pixel_valid = 0
+                    pred = np.nan
+                    
+                   
+                sum_water[0, y, x] = np.nansum(np.array([sum_water[0, y, x], pred]))
+                num_predictions[0, y, x] = np.sum(np.array([num_predictions[0, y, x], pixel_valid]))
 
         return sum_water, num_predictions